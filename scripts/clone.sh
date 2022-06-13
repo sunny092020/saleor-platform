211be29fc21a8f4aacb00da132a7715be4487cbc
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2022-06-13 21:59:38 +0700

    clone sub repo

---
+++ scripts/clone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/sunny092020/saleor.git
+git clone https://github.com/sunny092020/saleor-dashboard.git
+git clone https://github.com/sunny092020/react-storefront.git
