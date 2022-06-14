0d1ebde9cfec43f7821697073f46b7c48eb7e6d6
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2022-06-14 21:20:23 +0700

    add init data script

---
+++ scripts/init_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose run --rm api python3 manage.py migrate
+docker-compose run --rm api python3 manage.py collectstatic --noinput
+docker-compose run --rm api python3 manage.py populatedb --createsuperuser
