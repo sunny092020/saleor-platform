eda945437c6f42d8a5205c256017e911ae04d248
Vu Hoang Giang <itworkjp@gmail.com>
2022-06-17 10:48:58 +0700

    init merchant data

--- scripts/init_data.sh
+++ scripts/init_data.sh
@@ -3,3 +3,4 @@
 docker-compose run --rm api python3 manage.py migrate
 docker-compose run --rm api python3 manage.py collectstatic --noinput
 docker-compose run --rm api python3 manage.py populatedb --createsuperuser
+./scripts/init_pg.sh
