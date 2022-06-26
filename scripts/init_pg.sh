0b09e54e11a4ff49d93d5ea346808b7d00442ef0
Vu Hoang Giang <itworkjp@gmail.com>
2022-06-26 12:14:15 +0700

    add merchant groups

--- scripts/init_pg.sh
+++ scripts/init_pg.sh
@@ -10,5 +10,6 @@ DB_NAME="saleor"
 
 DB_PORT="5432"
 
-psql -h $DB_SERVER $DB_NAME -U $USER -w -c "\copy public.merchant_merchant ( user_id ) FROM 'data/merchant_merchant.csv' delimiter ',' csv header;"
-psql -h $DB_SERVER $DB_NAME -U $USER -w -c "\copy public.merchant_merchant_products ( merchant_id, product_id ) FROM 'data/merchant_merchant_products.csv' delimiter ',' csv header;"
+psql -h $DB_SERVER $DB_NAME -U $USER -w -c "\copy public.auth_group ( name ) FROM 'data/auth_group.csv' delimiter ',' csv header;"
+psql -h $DB_SERVER $DB_NAME -U $USER -w -c "\copy public.auth_group_permissions ( group_id, permission_id ) FROM 'data/auth_group_permissions.csv' delimiter ',' csv header;"
+psql -h $DB_SERVER $DB_NAME -U $USER -w -c "\copy public.account_user_groups ( user_id, group_id ) FROM 'data/account_user_groups.csv' delimiter ',' csv header;"
