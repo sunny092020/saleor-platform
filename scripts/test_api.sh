60f403fe03fc0d5b9d1c80a7752c11597fd051ea
Vu Hoang Giang <itworkjp@gmail.com>
2022-07-05 13:37:13 +0700

    fix csv test script

--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -1,4 +1,4 @@
 set -e;
 
 reset
-docker-compose run --rm api pytest --excelreport=report.xls --reuse-db -n0 saleor/ -s
+docker-compose run --rm api pytest --excelreport=report.xls --reuse-db -n0 saleor/graphql/gallery/tests/test_gallery.py::test_get_my_gallery -s
